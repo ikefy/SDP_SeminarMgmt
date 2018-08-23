8d62b79e507fae471079e15397dda78f8f71343a
ikefy <ike.yuan@outlook.com>
2018-08-23 15:48:50 +1000

    Created selectall script

---
+++ selectall.js
@@ -0,0 +1,47 @@
+var Connection = require('tedious').Connection;
+var Request = require('tedious').Request;
+
+// Create connection to database
+var config =
+   {
+       userName: 'adminsdp', // update me
+       password: 'p@ss1234', // update me
+       server: 'sdpseminarmgmt.database.windows.net', // update me
+       options:
+          {
+              database: 'SDP_SeminarMgmt' //update me
+             , encrypt: true
+          }
+   }
+var connection = new Connection(config);
+
+// Attempt to connect and execute queries if connection goes through
+connection.on('connect', function (err) {
+    if (err) {
+        console.log(err)
+    }
+    else {
+        queryDatabase()
+    }
+}
+ );
+
+function queryDatabase() {
+    console.log('Reading rows from the Table...');
+
+    // Read all rows from table
+    request = new Request(
+         "SELECT * FROM [dbo].[testtbl_1]'",
+            function (err, rowCount, rows) {
+                console.log(rowCount + ' row(s) returned');
+                process.exit();
+            }
+           );
+
+    request.on('row', function (columns) {
+        columns.forEach(function (column) {
+            console.log("%s\t%s", column.metadata.colName, column.value);
+        });
+    });
+    connection.execSql(request);
+}
