9982cffe7fd5eef3230d5d520811b50d5004a7a2
ikefy <ike.yuan@outlook.com>
2018-09-24 03:07:58 +1000

    created test endpoint

---
+++ routes/test.js
@@ -0,0 +1,21 @@
+﻿var router = require('express').Router();
+var TYPES = require('tedious').TYPES;
+
+/* GET task listing. */
+router.get('/', function (req, res) {
+
+    req.sql("SELECT * FROM [dbo].[testtbl_1]  for json path, without_array_wrapper")
+        .into(res, '{}');
+
+});
+
+/* GET single task. */
+router.get('/:id', function (req, res) {
+    
+    req.sql("SELECT * FROM [dbo].[testtbl_1] where ID = @id for json path, without_array_wrapper")
+        .param('ID', req.params.id, TYPES.nchar)
+        .into(res, '{}');
+
+});
+
+module.exports = router;
