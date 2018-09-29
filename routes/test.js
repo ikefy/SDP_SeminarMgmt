4bea9fa52ccdd1b196f98e07ddcce62ae71d9df1
ikefy <ike.yuan@outlook.com>
2018-09-29 22:28:50 +1000

    test

--- routes/test.js
+++ routes/test.js
@@ -2,7 +2,7 @@
 var TYPES = require('tedious').TYPES;
 
 /* GET test listing. */
-router.get('/', function (req, res) {
+router.query('/', function (req, res) {
 
     req.sql("SELECT * FROM [dbo].[testtbl_1]")
         .into(res, '[]');
@@ -21,7 +21,7 @@ router.get('/:id', function (req, res) {
 /* GET seminar listing. */
 router.get('/seminar', function (req, res) {
 
-    req.sql("SELECT * FROM [dbo].[seminar]")
+    req.sql("SELECT [SeminarID],[SeminarTitle],[SeminarDate],[SeminarStartTime],[SeminarEndTime],[SeminarStatus],[RoomID] FROM [dbo].[seminar]")
         .into(res, '[]');
 
 });
