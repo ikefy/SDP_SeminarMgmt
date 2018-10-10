ff5bce59b5f7b3fcaa0eead58018a2c7e0e95b8c
ikefy <ike.yuan@outlook.com>
2018-10-11 05:01:27 +1100

    removed daterange/sept params

--- routes/main.js
+++ routes/main.js
@@ -46,8 +46,6 @@ router.get('/seminar/:id/attendees', function (req, res) {
 router.get('/seminar/daterange/sept', function (req, res) {
 
     req.sql("SELECT * FROM [dbo].[seminar] where SeminarDate BETWEEN '2018-09-01' and '2018-09-30'")
-        .param('START', req.params.start, TYPES.Date)
-        .param('END', req.params.end, TYPES.Date)
         .into(res, '[]');
 
 });
