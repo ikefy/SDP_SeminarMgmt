8ad4834637f4e13c9e19c3cd49ac9904fde90f04
ikefy <ike.yuan@outlook.com>
2018-10-11 05:20:29 +1100

    test

--- routes/main.js
+++ routes/main.js
@@ -1,23 +1,7 @@
 ﻿var router = require('express').Router();
 var TYPES = require('tedious').TYPES;
-const axios = require('axios');
 
-
-axios.get('https://ikefy.github.io/SDP_SeminarMgmt/')
-    .then((response) => {
-        if (response.status === 200) {
-            const html = response.data;
-            const $ = cheerio.load(html);
-            // Get text 
-            console.log("------- with axios module -------")
-            console.log($.text());
-            // Get HTML 
-            //console.log($.html());
-        }
-    })
-    .catch((err) => {
-        throw new Error(err);
-    });
+router.get('/', function (req, res) {req.sql("SELECT * FROM [dbo].[testtbl_1] for json path").into(res, '[]');});
 
 /* GET user listing. */
 router.get('/api/user/', function (req, res) {
