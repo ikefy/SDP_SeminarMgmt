dc5ed878a3f5f4a5baf271ace74d6a81973287a7
ikefy <ike.yuan@outlook.com>
2018-10-11 02:56:38 +1100

    update

---
+++ routes/users.js
@@ -0,0 +1,21 @@
+﻿var router = require('express').Router();
+var TYPES = require('tedious').TYPES;
+
+/* GET seminar listing. */
+router.get('/seminar', function (req, res) {
+
+    req.sql("SELECT * FROM [dbo].[seminar] for json path")
+        .into(res, '[]');
+
+});
+
+/* GET single seminar. */
+router.get('/seminar/:id', function (req, res) {
+    
+    req.sql("SELECT * FROM [dbo].[seminar] where ID = @id for json path, without_array_wrapper")
+        .param('ID', req.params.id, TYPES.nchar)
+        .into(res, '[]');
+
+});
+
+module.exports = router;
