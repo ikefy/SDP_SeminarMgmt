d58d52dc2b30caf2ea000704dbf2a0621cb5505b
ikefy <ike.yuan@outlook.com>
2018-10-16 06:36:17 +1100

    update

---
+++ assets/js/seminar_list_seminardetail.js
@@ -0,0 +1,7 @@
+function showSeminarDetail(SeminarID) {
+    window.open('seminar_detail.html?'+SeminarID, '_self', false);
+}
+
+function getSeminarDetails(SeminarID) {
+
+}
--- assets/js/seminar_list_seminars - Copy.js
+++
@@ -1,3 +0,0 @@
-function showSeminarDetail(SeminarID) {
-
-}
