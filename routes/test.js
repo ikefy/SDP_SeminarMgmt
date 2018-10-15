aa7e82c217e6cbf06403c662b42356d3518a9cc4
ikefy <ike.yuan@outlook.com>
2018-10-16 04:57:23 +1100

    test

--- routes/test.js
+++ routes/test.js
@@ -1,7 +1,7 @@
 ﻿var router = require('express').Router();
 var TYPES = require('tedious').TYPES;
 
-router.get('/1', function (req, res) {req.sql("SELECT * FROM [dbo].[testtbl_1] for json path").into(res, '[]');});
+router.get('/1/', function (req, res) {req.sql("SELECT * FROM [dbo].[testtbl_1] for json path").into(res, '[]');});
 
 router.get('/test', function (req, res) {
     req.sql("INSERT INTO [dbo].[testtbl_1] ([ID],[FirstName],[LastName]) VALUES ('3','Z','Z')")
