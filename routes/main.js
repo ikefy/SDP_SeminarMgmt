35b6a202298e739716378f4e3586463706b5b1ec
ikefy <ike.yuan@outlook.com>
2018-10-16 11:29:01 +1100

    update

--- routes/main.js
+++ routes/main.js
@@ -39,7 +39,7 @@ router.get('/api/user/:id', function (req, res) {
         .into(res, '[]');
 });
 
-router.get('/api/userlogin/:username/:password', function (req, res) {
+router.get('/api/user/login/:username/:password', function (req, res) {
 
     req.sql("SELECT * FROM [dbo].[user] where UserLogin = @username and UserPassword = @password for json path, without_array_wrapper")
         .param('username', req.params.username, TYPES.nchar)
