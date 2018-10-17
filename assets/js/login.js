9b150c78944a85d41aacd426e9970331ae8c40eb
ikefy <ike.yuan@outlook.com>
2018-10-18 01:43:00 +1100

    test update

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,15 +1,15 @@
-function user_login(){
+function user_login() {
     var username = document.getElementById("username").value;
     var password = document.getElementById("password").value;
     console.log(username + password);
     getUser(username, password);
 }
 
-function getUser(u,p) {
+function getUser(username, password) {
     var settings = {
         "async": true,
         "crossDomain": true,
-        "url": "https://sdp-seminarmgmt.azurewebsites.net/api/user/login/" + u + '/' + p,
+        "url": "https://sdp-seminarmgmt.azurewebsites.net/api/user/login/" + username + '/' + password,
         "method": "GET",
         "headers": {
             "cache-control": "no-cache",
@@ -19,5 +19,21 @@ function getUser(u,p) {
 
     $.ajax(settings).done(function (response) {
         console.log(response);
+        var login_json = response;
+        var login_parse = JSON.parse(login_json);
+        localStorage.setItem("uid", login_parse.UserID);
+        localStorage.setItem("u", login_parse.UserLogin);
+        localStorage.setItem("p", login_parse.UserPassword);
+        console.log(localStorage.uid + localStorage.u + localStorage.p);
+        //uid = login_parse.UserID;
+        if (login_parse.UserID != null)
+            userLogin_btn();
+        else
+            console.log("wrong Login");
     });
+}
+
+function userLogin_btn() {
+    console.log("XX");
+
 }
