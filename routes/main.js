2b7f9f51acecaf0e70531174735db47207a12004
ikefy <ike.yuan@outlook.com>
2018-10-18 12:32:40 +1100

    added room endpoint

--- routes/main.js
+++ routes/main.js
@@ -150,4 +150,11 @@ router.get('/api/seminar/daterange/dec', function (req, res) {
         .into(res, '[]');
 });
 
+router.get('/api/room/:id', function (req, res) {
+
+    req.sql("SELECT * FROM [dbo].[room] where RoomID = 40000004 for json path, without_array_wrapper")
+        .param('ID', req.params.id, TYPES.nchar)
+        .into(res, '[]');
+});
+
 module.exports = router;
