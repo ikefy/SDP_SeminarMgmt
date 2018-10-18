1b31ceebd0d06ba5521654c6e18ada0160ff533e
ikefy <ike.yuan@outlook.com>
2018-10-18 11:06:24 +1100

    update

--- assets/js/seminar_create.js
+++ assets/js/seminar_create.js
@@ -22,7 +22,9 @@ function createSem() {
             "SeminarEndTime": document.getElementById("endTime").value,
             "SeminarStatus": "SOON",
             "SemRoomID": document.getElementById("roomNum").value,
-            "SemDescription": document.getElementById("semDescription").value
+            "SemDescription": document.getElementById("semDescription").value,
+            "SpeakerName": document.getElementById("speakerName").value,
+            "SpeakerDescription": document.getElementById("speakerDescription").value
         })
     }).done(function (response) {
         console.log(response + "sem created");
@@ -30,5 +32,25 @@ function createSem() {
 }
 
 function createBooking() {
-    
+    $.ajax({
+        async: true,
+        crossDomain: true,
+        url: "https://sdp-seminarmgmt.azurewebsites.net/api/booking",
+        method: "POST",
+        headers: {
+            'content-type': "application/json",
+            'cache-control': "no-cache",
+            'postman-token': "3c3e8eed-0d1a-3c80-1bd7-40adee5a8196"
+        },
+        processData: false,
+        data: JSON.stringify({
+            "BookingStatus": "BOOKED",
+            "SeminarID": document.getElementById("semDate").value,
+            "RoomID": document.getElementById("roomNum").value,
+            "UserID": document.getElementById("endTime").value,
+            "HostID": document.getElementById("endTime").value
+        })
+    }).done(function (response) {
+        console.log(response + "booking created");
+    });
 }
