088a66a437377fd6fa1faa82df527ff08a301ea1
ikefy <ike.yuan@outlook.com>
2018-10-18 11:12:14 +1100

    test

--- routes/main.js
+++ routes/main.js
@@ -95,8 +95,7 @@ router.post('/api/seminar/', function (req, res) {
     .param('SemDescription', req.body.SemDescription, TYPES.nchar)
     .param('SpeakerName', req.body.SpeakerName, TYPES.nchar)
     .param('SpeakerDescription', req.body.SpeakerDescription, TYPES.nchar)
-        .exec(res)
-    .into(res, '[]');
+        .exec(res);
 });
 
 /* GET single seminar. */
