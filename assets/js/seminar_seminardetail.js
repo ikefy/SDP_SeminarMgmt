2c378c05f95389f49f5dcc25a261b27ed0c383ed
ikefy <ike.yuan@outlook.com>
2018-10-18 12:43:01 +1100

    update

--- assets/js/seminar_seminardetail.js
+++ assets/js/seminar_seminardetail.js
@@ -29,7 +29,28 @@ function getSeminarDetails(SeminarID) {
         var seminar = response;
         var seminar_parse = JSON.parse(seminar);
         var seminar_length = seminar_parse.length;
+        getRoomNumber(seminar_parse.SemRoomID);
         setPageValues(seminar_parse);
+    });
+}
+
+function getRoomNumber(roomID) {
+    var settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": "https://sdp-seminarmgmt.azurewebsites.net/api/room/" + roomID,
+        "method": "GET",
+        "headers": {
+            "cache-control": "no-cache",
+            "postman-token": "f004739d-45dc-9ee3-183a-61c9851ff4db"
+        }
+    }
+
+    $.ajax(settings).done(function (response) {
+        console.log(response);
+        var room = response;
+        var room_parse = JSON.parse(room);
+        document.getElementById("semVenue").innerHTML = room_parse.RoomNumber;
     });
 }
 
@@ -37,7 +58,6 @@ function setPageValues(seminar) {
     document.getElementById("semTitle").innerHTML = seminar.SeminarTitle;
     document.getElementById("semDate").innerHTML = seminar.SeminarDate + '<br>' + seminar.SeminarStartTime + ' to ' + seminar.SeminarEndTime;
     document.getElementById("semDescription").innerHTML = seminar.SemDescription;
-    document.getElementById("semVenue").innerHTML = seminar.SemRoomID;
     document.getElementById("semHost").innerHTML = seminar.SpeakerName;
     document.getElementById("hostDescription").innerHTML = seminar.SpeakerDescription;
 }
